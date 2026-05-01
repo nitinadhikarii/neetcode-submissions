6dd6e654185ab27e2398aa9856248dee20113db0
nitin ad <bbno924@gmail.com>
2026-05-01 22:11:52 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hm={}
+        lis=[]
+        for i in range(len(strs)):
+
+            if ("".join(sorted(strs[i]))) not in hm.keys():
+                hm["".join(sorted(strs[i]))]=[strs[i]]
+            else:    
+                (hm["".join(sorted(strs[i]))]).append(strs[i])
+
+        for i in hm.keys():
+            lis.append(hm[i])
+        return lis        
+
+
+
+        
